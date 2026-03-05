3b5ef9279c9d9e77ab564c559c4d7a5b9976be94
Obafunsho E. Thanni <thannio@myumanitoba.ca>
2026-03-05 12:13:58 -0600

    home page fix

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,16 +20,11 @@ LINKS = (
     ("Pelican", "https://getpelican.com/"),
     ("Python.org", "https://www.python.org/"),
     ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
-
-# Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
 )
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
+DISPLAY_CATEGORIES_ON_MENU = False
+ARTICLE_METADATA = False
